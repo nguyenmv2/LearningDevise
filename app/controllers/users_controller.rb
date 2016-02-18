0f261c085a3d7f9d804bbf0910aeb2ca79712bae
My Nguyen <nguyenmv2@hendrix.edu>
2016-02-18 00:56:11 -0600

    Can now log out of current account. Nothing was wrong, I'm just dumb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
+
   def show
     @user = User.find(params[:id])
+
   end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to home, :notice => 'User deleted'
+  end
+
 end
